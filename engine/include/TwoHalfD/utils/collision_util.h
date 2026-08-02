d292eba4f22e4b9946cc800f4a91c7b9677ae92d
erikweinberger <mumeriken123@gmail.com>
2026-08-02 14:53:45 +1000

    add function description for resolveCircleVsCircle

--- engine/include/TwoHalfD/utils/collision_util.h
+++ engine/include/TwoHalfD/utils/collision_util.h
@@ -8,7 +8,7 @@ namespace TwoHalfD {
 // for circle to move along to no longer overlap
 XYVectorf resolveCircleVsSegment(XYVectorf pos, float radius, const Segment &seg);
 
-// Calculates circle overlap
+// Calculates circle overlap and give vector to move circle one so circles no longer overlap
 XYVectorf resolveCircleVsCircle(XYVectorf pos, float r1, XYVectorf otherPos, float r2);
 } // namespace TwoHalfD
 
